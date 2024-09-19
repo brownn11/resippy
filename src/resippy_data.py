22d5251faff1b7c4bf2f70f7204e77c536e4fc18
Nina Brown <103523018+brownn11@users.noreply.github.com>
2024-09-19 11:14:26 -0500

    Create resippy_data.py
    Added information for recipes and ingredients.

---
+++ src/resippy_data.py
@@ -0,0 +1,137 @@
+# big ole book of recipes
+
+recipes = {'Buddha bowls': # measurements are highly intuitive, plus most are difficult to buy in specific portions -- go with your heart
+                       {'white rice':1, 'peanut butter':1, 'tofu':1, 'edamame':1, 'broccoli':1, 'brussel sprout':1, 'green onion':1,
+                        'soy sauce':1, 'sesame oil':1, 'rice vinegar':1, 'lemon juice':1, 'lemon zest':1, 'garlic powder':1, # sauce one
+                        'kewpie mayo':1},                                                                                    # sauce two
+           
+          'Pao pao udon': # measurements are again mostly with your heart, except the sauce, which is 1:1:1
+                       {'udon noodle':2, 'broccoli':2, 'onion':0.5, 'bell pepper':1, 'garlic':2, 'tofu':1, 'kewpie mayo':1, 'condensed sweetened milk':1, 'soy sauce':1}, # sauce is 1:1:1
+          
+          'Hamburgers and fries': # emily has in recipe book
+                       {'ground beef':2, 'worschester sauce':1, 'garlic':1, 'buns':6, # burgers
+                        'russet potatos':8, 'kewpie mayo':1, 'lemon juice':1, 'lemon zest':1, 'seeded mustard':1, 'garlic':2, 'sriracha':1}, # fries + sauce
+           
+          'Pizza': # emily has in recipe book
+                       {'flour':4, 'garlic':1, 'garlic powder':1, 'olive oil':1,                # dough
+                        'pizza sauce':1, 'pesto':1, 'mozzarella cheese':2,                      # base
+                        'tomato':1, 'garlic':2, 'salami':1, 'bell pepper':0.25, 'onion':0.5},   # toppings
+           
+          'Chorizo pasta': # measurements are mostly by heart
+                       {'pasta':1, 'chorizo':1, 'bell pepper':0.5, 'onion':0.5, 'garlic':3, 'carrot':0.5, 'tomato sauce':1, 'shredded parmesan':1},
+          
+          'Mac and cheese': # measurements are mostly by heart
+                       {'cheddar cheese':1, 'gouda cheese':1, 'asiago cheese':1, 'macaroni':2, 'mustard powder':0.5, 'cornstarch':0.5, 'butter':0.5, 'milk':0.5},
+          
+          'Spaghetti bolognese': # measurements are mostly by heart
+                       {'spaghetti':1, 'tomato sauce':1, 'garlic':3, 'onion':0.5, 'ground beef':2},
+           
+          'Quesadillas and guac': # measurements are fully by heart
+                       {'tortilla':4, 'mexican cheese':2, 'tempeh':1, 'onion':1, 'bell pepper':0.5, 'tomato':1, 'chalula':1, # quesadilla
+                        'tortilla chip':1, 'avocado':5, 'garlic':3, 'jalepeno':2, 'salt':1},                                 # chip + guac (inc. tomato and onion from quesadillas)
+          
+          'Stuffed bell pepper': # https://www.budgetbytes.com/stuffed-bell-peppers/
+                       {'bell pepper': 4, 'ground beef':2, 'olive oil':1, 'onion':1, 'garlic':3, 'garlic powder':0.5, 'tomato sauce':1, 'white rice':0.5, 'broth':1, 'mozzarella cheese':1},
+           
+           'Pasta and meatballs': # simple pasta and meatballs -- use frozen meatballs to simplify
+                       {'pasta':1, 'tomato sauce':1, 'shredded parmesan':1, 'ground beef':1, 'breadcrumb':1, 'egg':2, 'garlic':2, 'milk':0.25, 'onion':0.25},
+           
+           'Tortellini': # very simple tortelini
+                       {'pre-made tortellini':2, 'tomato sauce':1, 'shredded parmesan':1},
+           
+           'Tortellini soup': # https://www.budgetbytes.com/spinach-tortellini-soup/
+                       {'pre-made tortellini':2, 'tomato paste':1, 'shredded parmesan':1, ' heavy cream':1, 'spinach':2, 'broth':4, 'garlic':4, 'onion':1, 'carrot':2, 'celery':2, 'crushed tomato':1},
+           
+           'Curried tofu': # by heart
+                       {'tofu':1, 'curry sauce':1, 'onion':1, 'bell pepper':1, 'carrot':1, 'broccoli':1, 'white rice':2, 'garbanzo bean':1, 'garlic':2},
+           
+           'Stir-fried tofu': # by heart
+                       {'tofu':1, 'onion':1, 'bell pepper':1, 'carrot':1, 'broccoli':1, 'white rice':2, 'egg':3, 'soy sauce':1, 'rice vinegar':1, 'sesame oil':1, 'garlic':3, 'sriracha':2},
+           
+           'Ramen': # by heart
+                       {'egg':2, 'onion':1, 'green onion':1, 'carrot':1, 'broccoli':1, 'ramen':3, 'soy sauce':1, 'miso paste':1, 'sesame oil':1, 'garlic':3, 'gochujang':1},
+        
+          # mushroom risotto
+           
+          # burritos/tacos + guac
+           
+          # sesame tofu
+           
+          # sweet + sour tofu
+                      
+          # chili
+           
+          # chicken and dumplings
+          }
+
+
+
+# meal-type organized:
+
+# "Lighter" meals
+recipes_light = {'Buddha bowls': # measurements are highly intuitive, plus most are difficult to buy in specific portions -- go with your heart
+                       {'white rice':1, 'peanut butter':1, 'tofu':1, 'edamame':1, 'broccoli':1, 'brussel sprout':1, 'green onion':1,
+                        'soy sauce':1, 'sesame oil':1, 'rice vinegar':1, 'lemon juice':1, 'lemon zest':1, 'garlic powder':1, # sauce one
+                        'kewpie mayo':1},                                                                                    # sauce two
+           
+          'Pao pao udon': # measurements are again mostly with your heart, except the sauce, which is 1:1:1
+                       {'udon noodle':2, 'broccoli':2, 'onion':0.5, 'bell pepper':1, 'garlic':2, 'tofu':1, 'kewpie mayo':1, 'condensed sweetened milk':1, 'soy sauce':1}, # sauce is 1:1:1
+          
+          'Chorizo pasta': # measurements are mostly by heart
+                       {'pasta':2, 'chorizo':1, 'bell pepper':0.5, 'onion':0.5, 'garlic':3, 'carrot':0.5, 'tomato sauce':1, 'shredded parmesan':1},
+          
+          'Spaghetti bolognese': # measurements are mostly by heart
+                       {'spaghetti':2, 'tomato sauce':1, 'garlic':3, 'onion':0.5, 'ground beef':2},
+           
+          'Quesadillas and guac': # measurements are fully by heart
+                       {'tortilla':4, 'mexican cheese':2, 'tempeh':1, 'onion':1, 'bell pepper':0.5, 'tomato':1, 'chalula':1, # quesadilla
+                        'tortilla chip':1, 'avocado':5, 'garlic':3, 'jalepeno':2, 'salt':1},                                 # chip + guac (inc. tomato and onion from quesadillas)
+          
+          'Stuffed bell pepper': # https://www.budgetbytes.com/stuffed-bell-peppers/
+                      {'bell pepper': 4, 'ground beef':2, 'olive oil':1, 'onion':1, 'garlic':3, 'garlic powder':0.5, 'tomato sauce':1, 'white rice':0.5, 'broth':1, 'mozzarella cheese':1},
+                 
+          # tortllini soup
+                 
+          # ramen 
+                   
+          # burritos/tacos + guac
+           
+          # sesame tofu
+           
+          # sweet + sour tofu
+           
+          # curried tofu
+                      
+          # stir-fry tofu (w lots of veggies)
+           
+          # simple tortelini
+           
+          # simple pasta and meatballs 
+           
+          }
+
+# "Unhealthy" meals
+recipes_heavy = {'Hamburgers and fries': # emily has in recipe book
+                       {'ground beef':2, 'worschester sauce':1, 'garlic':1, 'buns':6, # burgers
+                        'russet potatos':8, 'kewpie mayo':1, 'lemon juice':1, 'lemon zest':1, 'seeded mustard':1, 'garlic':2, 'sriracha':1}, # fries + sauce
+           
+          'Pizza': # emily has in recipe book
+                       {'flour':4, 'garlic':1, 'garlic powder':1, 'olive oil':1,                # dough
+                        'pizza sauce':1, 'pesto':1, 'mozzarella cheese':2,                      # base
+                        'tomato':1, 'garlic':2, 'salami':1, 'bell pepper':0.25, 'onion':0.5},   # toppings
+          
+          'Mac and cheese': # measurements are mostly by heart
+                       {'cheddar cheese':1, 'gouda cheese':1, 'asiago cheese':1, 'macaroni':2, 'mustard powder':0.5, 'cornstarch':0.5, 'butter':0.5, 'milk':0.5},
+          
+          }
+
+# Energy/ingredient-intensive meals
+
+recipes_intense = {
+          # mushroom risotto
+
+          # chili
+
+          # chicken and dumplings
+
+          }
